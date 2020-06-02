d85e31b24f5e040ccf19c0a2bf8849b31d15741f
Dominic <48520590+Sevendaye@users.noreply.github.com>
2020-06-02 16:13:55 +0200

    add formulaire|message components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Formulaire from './components/Formulaire';
+import Message from './components/Messages';
 
 class App extends Component {
   render() {
 
     return (
       <div className="box">
+        <div>
+          <div className='messages'>
+            <Message />
+            <Message />
+            <Message />
+          </div>
+        </div>
+        <Formulaire />
       </div>
     );
   }
---
+++ src/components/Messages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Messages() {
+    return (
+        <p className='user-message'>Mon message</p>
+    )
+}
+
+export default Messages
